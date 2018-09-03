44954190dfac9881da4a39297c5e0c843646ba46
Phalanx <meeS33ks@protonmail.com>
2018-09-03 09:35:24 -0600

    Override RankedGradeBook's GetLetterGrade

--- GradeBook/GradeBooks/RankedGradeBook .cs
+++ GradeBook/GradeBooks/RankedGradeBook .cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace GradeBook.GradeBooks
 {
@@ -9,6 +10,49 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             Type = Enums.GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if(Students.Count < 5)
+            {
+                throw new InvalidOperationException("Ranked grading requires at least 5 students.");
+            }
+
+            Students.OrderBy(students => students.AverageGrade).ToList();
+            Student targetStudent;
+            var ranking = 0;
+
+            foreach (Student student in Students)
+            {
+                if (student.AverageGrade == averageGrade)
+                {
+                    targetStudent = student;
+                    ranking = Students.IndexOf(targetStudent)+1;
+                    break;
+                }
+            }
+
+            if(ranking > (Students.Count *.8))
+            {
+                return 'A';
+            }
+            else if (ranking > (Students.Count *.6))
+            {
+                return 'B';
+            }   
+            else if (ranking > (Students.Count *.4))
+            {
+                return 'C';
+            } 
+            else if (ranking > (Students.Count *.2))
+            {
+                return 'D';
+            }
+            else
+            {
+                return 'F';
+            }
         }
     }
 }
